f9c30662aba304297d2dbc94e2d16bc85c11a270
Manan21st <manan9346@gmail.com>
2024-07-03 10:07:36 +0530

    post call

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json()); // Add this line to parse JSON data in the request body
+
 let courses = [
         {id:1, name:'java'},
         {id:2, name:'python'}, 
         {id:3, name:'c++'}
+];
+
+let instructors = [
+    {id:1, name:'Siddharth'},
+    {id:2, name:'Kshitij'},
+    {id:3, name:'Dhruv'}
 ];
 
 app.listen(3000, () => {
@@ -12,9 +20,19 @@ app.listen(3000, () => {
 });
 
 app.get('/', (req, res) => {
-
+    res.send('Hello World');
 });
 
 app.get('/courses', (req, res) => {
+    res.json(courses);
+});
+
+app.get('/instructors', (req, res) => {
+    res.json(instructors);
+});
+
+app.post('/courses', (req, res) => {
+    const newCourse = req.body;
+    courses.push(newCourse);
     res.json(courses);
 });
